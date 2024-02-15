6d03f973acfc845b0cb56852be52d544b4f6cf56
rahulthakur <mailrahulkthakur@gmail.com>
2024-02-15 22:53:34 +0530

    34 - Create Post Model Class

---
+++ site/src/commonMain/kotlin/com/yogaveda/models/Post.kt
@@ -0,0 +1,18 @@
+@file:Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
+
+package com.yogaveda.models
+
+expect class Post {
+
+    val id: String
+    val author: String
+    val date: Long
+    val title: String
+    val subtitle: String
+    val thumbnail: String
+    val content: String
+    val category: Category
+    val popular: Boolean
+    val main: Boolean
+    val sponsored: Boolean
+}
---
+++ site/src/jvmMain/kotlin/com/yogaveda/models/Post.kt
@@ -0,0 +1,23 @@
+@file:Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
+
+package com.yogaveda.models
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import org.bson.codecs.ObjectIdGenerator
+
+@Serializable
+actual data class Post(
+    @SerialName("_id")
+    actual val id: String = ObjectIdGenerator().generate().toString(),
+    actual val author: String,
+    actual val date: Long,
+    actual val title: String,
+    actual val subtitle: String,
+    actual val thumbnail: String,
+    actual val content: String,
+    actual val category: Category,
+    actual val popular: Boolean = false,
+    actual val main: Boolean = false,
+    actual val sponsored: Boolean = false
+)
